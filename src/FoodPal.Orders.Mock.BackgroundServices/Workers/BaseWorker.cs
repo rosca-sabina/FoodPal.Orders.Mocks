0166301259d72c25484c4a58d59d172e248ad70e
Sabina Rosca <7324042-sabinarosca97@users.noreply.gitlab.com>
2021-02-21 19:47:34 +0200

    Add project files.

---
+++ src/FoodPal.Orders.Mock.BackgroundServices/Workers/BaseWorker.cs
@@ -0,0 +1,39 @@
+﻿using FoodPal.Orders.Mock.Dtos;
+using FoodPal.Orders.Mock.MessageBroker;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodPal.Orders.Mock.BackgroundServices.Workers
+{
+	public abstract class BaseWorker
+	{
+		protected readonly IMessageBroker MessageBroker;
+
+		public BaseWorker(IMessageBroker messageBroker)
+		{
+			MessageBroker = messageBroker;
+		}
+
+		protected async Task ProcessMessageAsync(string messageEnvelopeAsString, string providerName, int secondsRequiredForProcessing)
+		{
+			try
+			{
+				var payload = JsonConvert.DeserializeObject<MessageEnvelope<MessageBrokerOrderRequestDto>>(messageEnvelopeAsString);
+
+				await Task.Delay(TimeSpan.FromSeconds(secondsRequiredForProcessing));
+				await MessageBroker.SendMessageAsync($"provider-{providerName}-response",
+					new MessageEnvelope<MessageBrokerOrderResponseDto>("order-completed", new MessageBrokerOrderResponseDto
+					{
+						OrderId = payload.Data.OrderId,
+						OrderItemIds = payload.Data.OrderItems.Select(x => x.OrderItemId).ToList()
+					}));
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("Message processing failed.", ex);
+			}
+		}
+	}
+}
---
+++ src/FoodPal.Orders.Mock.ChefsExperience.Api/Controllers/ApiBaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodPal.Orders.Mock.ChefsExperience.Api.Controllers
+{
+	[ApiController]
+	[Produces("application/json")]
+	public class ApiBaseController : ControllerBase
+	{
+	}
+}
